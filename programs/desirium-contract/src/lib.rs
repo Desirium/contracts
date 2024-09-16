3cd98f22d900b907af0cc84e3892bab1729e6bb8
Oleg Syniakevych <olegsyniakevych@gmail.com>
2024-09-16 20:19:34 +0300

    Project boilerplate

---
+++ programs/desirium-contract/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("CzY7h2jY3Fq8Rw3rSSi8PZksgbtZzn44aQR6EeYm2bMo");
+
+#[program]
+pub mod desirium_contract {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
